fce21e4f51b4ebbe0ee137f5ca5c53fbd990601d
Diones <pereiradiones987@gmail.com>
2025-06-18 14:26:02 -0300

    Criando o Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Logo } from "@/components/Logo"
+import { ThemeToggle } from "./theme-toggle"
 
 export const Header = () => {
   return (
-    <header>
-      
+    <header className="flex justify-between items-center my-5 mx-3">
+      <div className="flex items-center gap-3">
+        <Logo />
+        <ThemeToggle />
+      </div>
+      <div className="flex items-center gap-3">
+
+      </div>
     </header>
   )
 }
