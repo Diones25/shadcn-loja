16d830a28383146464d8eba73385952fad58daee
Diones <pereiradiones987@gmail.com>
2025-06-26 21:58:39 -0300

    Atualizando quantidade

--- src/components/cart/item-quantity.tsx
+++ src/components/cart/item-quantity.tsx
@@ -1,13 +1,33 @@
+import { useCartStore } from "@/stores/cart-store"
 import { Cart } from "@/types/cart"
+import { Button } from "../ui/button"
+import { MinusIcon, PlusIcon } from "lucide-react"
 
 type Props = {
   cartItem: Cart
 }
 
 const CartItemQuantity = ({ cartItem }: Props) => {
+  const { upsertCartItem } = useCartStore(state => state)
   return (
-    <div>
-      {cartItem.quantity}
+    <div className="flex items-center gap-2">
+      <Button
+        variant={"outline"}
+        size={"icon"}
+        className="size-6 cursor-pointer"
+        onClick={() => upsertCartItem(cartItem.product, 1)}
+      >
+        <PlusIcon className="size-3" />
+      </Button>
+      <div className="text-xs">{cartItem.quantity}</div>
+      <Button
+        variant={"outline"}
+        size={"icon"}
+        className="size-6 cursor-pointer"
+        onClick={() => upsertCartItem(cartItem.product, -1)}
+      >
+        <MinusIcon className="size-3" />
+      </Button>
     </div>
   )
 }
