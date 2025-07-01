f989766b2eaeb660952b2e80be2ecc4f6281b804
Diones <pereiradiones987@gmail.com>
2025-07-01 14:39:51 -0300

    Enviando pro WhatsApp

--- src/components/checkout/step-finish.tsx
+++ src/components/checkout/step-finish.tsx
@@ -1,9 +1,21 @@
+import { useCheckoutStore } from '@/stores/checkout-store'
 import React from 'react'
+import { Button } from '../ui/button'
+import Link from 'next/link'
 
 const StepFinish = () => {
+  const { name } = useCheckoutStore(state => state)
+
+  const message = '';
+  const linkZap = `https://wa.me//${process.env.NEXT_PUBLIC_ZAP}?text=${encodeURI(message)}`;
+
   return (
-    <div>
-      
+    <div className='text-center flex flex-col gap-5'>
+      <p>Perfeito <strong>{name}</strong>!</p>
+      <p>Agora envie seu pedido para nosso Whatsapp para concluir. Nosso atendente irá te guiar sobre o andamento do pedido.</p>
+      <Button>
+        <Link target='_blank' href={linkZap} >Enviar para o Whatsapp</Link>
+      </Button>
     </div>
   )
 }
